d940d41a0314b1983db44887b373c637918a7168
Ling Tan <lingt.xyz@gmail.com>
2020-06-22 17:57:56 -0400

    add duplicated removing

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	f := flag.String("f", "", "input file name")
 	flag.Parse()
 	normalized(*f)
+	//removeDuplicates(*f)
 }
 
 var reMem = regexp.MustCompile(`MEM\w+`)
@@ -22,7 +23,6 @@ func normalized(fileName string) {
 		log.Fatalf("Cannot open file: %v, got error: %v", fileName, err)
 	}
 	defer sourceFile.Close()
-
 
 	targetFileName := fileName + ".normalized"
 	targetFile, err := os.Create(targetFileName)
@@ -42,5 +42,36 @@ func normalized(fileName string) {
 		s = reMem.ReplaceAllString(s, "MEM")
 		s = reTmp.ReplaceAllString(s, "TMP")
 		_, _ = targetFile.WriteString(s)
+	}
+}
+
+func removeDuplicates(fileName string){
+	sourceFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Cannot open file: %v, got error: %v", fileName, err)
+	}
+	defer sourceFile.Close()
+
+	targetFileName := fileName + ".duplicatesRemoved"
+	targetFile, err := os.Create(targetFileName)
+	if err != nil {
+		log.Fatalf("Failed to create target file: %v, got error: %v", targetFileName, err)
+	}
+	defer targetFile.Close()
+
+	reader := bufio.NewReader(sourceFile)
+
+	set := make(map[string]bool) // New empty set
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		set[s] = true            // Add
+	}
+
+	for k := range set {         // Loop
+		_, _ = targetFile.WriteString(k)
 	}
 }
